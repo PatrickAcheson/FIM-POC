55fe5507be685a92453a0db672260b87588aa9ae
Patrick <patrick.acheson@hotmail.com>
2023-03-22 20:51:33 +0000

    updates

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import os
-import hashlib
+import sys
 import json
+import hashlib
+import time
 from pathlib import Path
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 
 def generate_file_hash(file_path):
     sha256_hash = hashlib.sha256()
@@ -22,19 +26,65 @@ def get_file_info(file_path):
         "permissions": oct(file_stats.st_mode)[-3:]
     }
 
-def generate_baseline(directory, output_file):
-    baseline_data = []
+def load_baseline(file_path):
+    with open(file_path, "r") as f:
+        return json.load(f)
 
-    for root, _, files in os.walk(directory):
-        for file in files:
-            file_path = Path(root) / file
-            baseline_data.append(get_file_info(file_path))
+def detect_changes(file_path, baseline_data):
+    current_file_info = get_file_info(file_path)
 
-    with open(output_file, "w") as f:
-        json.dump(baseline_data, f, indent=2)
+    for file_info in baseline_data:
+        if file_info["path"] == current_file_info["path"]:
+            if file_info["hash"] != current_file_info["hash"]:
+                print(f"File content changed: {file_path}")
+            if file_info["permissions"] != current_file_info["permissions"]:
+                print(f"File permissions changed: {file_path}")
+            return
+
+    print(f"New file detected: {file_path}")
+
+class FileChangeHandler(FileSystemEventHandler):
+    def __init__(self, baseline_data):
+        self.baseline_data = baseline_data
+
+    def on_modified(self, event):
+        if not event.is_directory:
+            print(f"File modified: {event.src_path}")
+            detect_changes(event.src_path, self.baseline_data)
+
+    def on_created(self, event):
+        if not event.is_directory:
+            print(f"File created: {event.src_path}")
+            detect_changes(event.src_path, self.baseline_data)
+
+    def on_deleted(self, event):
+        if not event.is_directory:
+            print(f"File deleted: {event.src_path}")
+
+def monitor_directory(directory, baseline_data):
+    event_handler = FileChangeHandler(baseline_data)
+    observer = Observer()
+    observer.schedule(event_handler, directory, recursive=True)
+    observer.start()
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+
+    observer.join()
 
 if __name__ == "__main__":
-    directory = "./"
-    output_file = "baseline_data.json"
-    generate_baseline(directory, output_file)
-    print(f"Baseline generated and saved to {output_file}")
+    
+    while True:
+        directory = input("Enter a file path to monitor or type 'exit' to quit: ")
+        if directory.lower() == 'exit':
+            break
+        if os.path.exists(directory):
+            baseline_file = "baseline_data.json"
+            baseline_data = load_baseline(baseline_file)
+            print(f"Monitoring directory: {directory}")
+            monitor_directory(directory, baseline_data)
+        else:
+            print(f"The directory '{directory}' does not exist. Please enter a valid path.")
