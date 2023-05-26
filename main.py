c09f58891f8b4620c7b13ac76b321d4aa7b4ba39
PatrickAcheson <patrick.acheson@hotmail.com>
2023-05-26 19:35:55 +0100

    refactored for efficiency and reducingredundancy

--- main.py
+++ main.py
@@ -9,27 +9,37 @@ from watchdog.events import FileSystemEventHandler
 def generate_file_hash(file_path):
     sha256_hash = hashlib.sha256()
     with open(file_path, "rb") as f:
-        for byte_block in iter(lambda: f.read(4096), b""): sha256_hash.update(byte_block)
+        for byte_block in iter(
+            lambda: f.read(4096), b""): sha256_hash.update(byte_block)
     return sha256_hash.hexdigest()
 
 # Function to get file information (path, hash, size, permissions)
 def get_file_info(file_path):
     file_stats = os.stat(file_path)
-    return {"path": str(file_path), "hash": generate_file_hash(file_path), "size": file_stats.st_size, "permissions": oct(file_stats.st_mode)[-3:]}
+    return {"path": str(file_path),
+            "hash": generate_file_hash(file_path),
+            "size": file_stats.st_size,
+            "permissions": oct(file_stats.st_mode)[-3:]}
 
 # Function to load baseline data from a JSON file
 def load_baseline(file_path):
-    with open(file_path, "r") as f: return json.load(f)
+    with open(file_path, "r") as f:
+        return json.load(f)
 
 # Function to save baseline data to a JSON file
 def save_baseline(file_path, baseline_data):
-    with open(file_path, "w") as f: json.dump(baseline_data, f, indent=2)
+    with open(file_path, "w") as f:
+        json.dump(baseline_data, f, indent=2)
 
 # Function to log change in the system log file
 def log_change(date, change_type, file_path):
-    with open(f'sys_log_{date}', 'a') as f: f.write(f"{datetime.now().strftime('%Y-%m-%d_%H:%M:%S.%f')} {change_type} {file_path}\n")
+    with open(f'sys_log_{date}', 'a') as f:
+        f.write(
+            f"{datetime.now().strftime('%Y-%m-%d_%H:%M:%S.%f')} {change_type} {file_path}\n")
 
 # Function to detect changes to a file and log them
+
+
 def detect_changes(file_path, baseline_data, change_type):
     current_file_info = get_file_info(file_path)
     day = datetime.now().strftime("%F")
@@ -49,31 +59,49 @@ def detect_changes(file_path, baseline_data, change_type):
 
 # Class to handle file system events
 # https://pythonhosted.org/watchdog/api.html#event-classes
+
+
 class FileChangeHandler(FileSystemEventHandler):
-    def __init__(self, baseline_data): self.baseline_data = baseline_data
-    def on_modified(self, event): detect_changes(event.src_path, self.baseline_data, "File modified:") if not event.is_directory else None
-    def on_created(self, event): detect_changes(event.src_path, self.baseline_data, "File created:") if not event.is_directory else None
-    def on_deleted(self, event): detect_changes(event.src_path, self.baseline_data, "File deleted:") if not event.is_directory else None
+    def __init__(self, baseline_data):
+        self.baseline_data = baseline_data
+
+    def on_modified(self, event):
+        if not event.is_directory:
+            detect_changes(event.src_path, self.baseline_data, "File modified:")
+
+    def on_created(self, event):
+        if not event.is_directory:
+            detect_changes(event.src_path, self.baseline_data, "File created:")
+
+    def on_deleted(self, event):
+        if not event.is_directory:
+            detect_changes(event.src_path, self.baseline_data, "File deleted:")
 
 # Function to monitor a directory
 def monitor_directory(directory, baseline_data):
     observer = Observer()
-    observer.schedule(FileChangeHandler(baseline_data), directory, recursive=True)
+    observer.schedule(FileChangeHandler(baseline_data),
+                      directory, recursive=True)
     observer.start()
     try:
-        while True: pass
-    except KeyboardInterrupt: observer.stop()
+        while True:
+            pass
+    except KeyboardInterrupt:
+        observer.stop()
     observer.join()
+
 
 # Main function
 if __name__ == "__main__":
     while True:
         directory = input("Enter a file path to monitor or type 'q' to quit: ")
-        if directory.lower() == 'q': break
+        if directory.lower() == 'q':
+            break
         if os.path.exists(directory):
             baseline_file = "baseline_data.json"
             baseline_data = load_baseline(baseline_file)
             print(f"Monitoring directory: {directory}")
             monitor_directory(directory, baseline_data)
         else:
-            print(f"The directory '{directory}' does not exist. Please enter a valid path.")
+            print(
+                f"The directory '{directory}' does not exist. Please enter a valid path.")
